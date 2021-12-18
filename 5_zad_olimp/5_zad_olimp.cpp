ad0dca86712f4f41b3b067eabfc64c44ec3722d5
shevczik <94609661+shevczik@users.noreply.github.com>
2021-12-18 19:19:31 +0300

    Add files via upload

--- 5_zad_olimp/5_zad_olimp.cpp
+++ 5_zad_olimp/5_zad_olimp.cpp
@@ -1,20 +1,69 @@
-﻿// 5_zad_olimp.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
+﻿
+#include <vector>
+#include <string>
 #include <iostream>
+using namespace std;
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    short x,y,amb_x =-1,amb_y=-1,size,koef;
+    //1 строка
+    cin >> y;
+    cin >> x;
+    cin >> amb_x;
+    cin >> amb_y;
+    cin >> size;
+    cin >> koef;
+
+    vector <vector <int>> field_cost;
+    field_cost.resize(x);
+    for (int i=0; i < x; i++) { //установление размерности матрицы
+        field_cost[i].resize(y);
+    }
+    vector <vector<int>> field_mass = field_cost;
+    vector <vector <int>> field_price = field_cost;
+    for (int i = 0; i < x; i++) { // заполнение вектора обьема клеток
+        for (int k = 0; k < y; k++) {
+            short num;
+            cin >> num;
+            field_mass[i][k] = num;
+        }
+    }
+
+    for (int i = 0; i < x; i++) { // заполнение вектора цены за тону на клетке
+        for (int k = 0; k < y; k++) {
+            short num;
+            cin >> num;
+            field_cost[i][k] = num;
+        }
+    }
+    
+    for (int i = 0; i < x; i++) { // заполнение вектора стоимости пукупки + доставки на клетке
+        for (int k = 0; k < y; k++) {
+            field_price [i][k] = field_cost[k][i]*field_mass[k][i];//цена покупки всего урожая
+            short dostavka=koef*(abs(amb_x-k)+abs(amb_y-i)); // цена доставки до амбара
+            field_price[i][k] += dostavka;
+        }
+    }
+    unsigned int cost = 0 ,mass=0;
+
+
+    short min=10000, min_x, min_y;
+    for (mass; mass < size;) {
+        for (int i = 0; i < x; i++) {
+            for (int k = 0; k < y; k++) {
+                if (field_price[i][k] < min) {
+                    if (i != amb_x && k != amb_y) {
+                        min = field_price[i][k];
+                        field_price[i][k] = 100000;
+                    }
+                }
+            }
+        }
+        mass += min;
+    }
+    cout << mass - min;
+    // сортировать ничего нельзя ,т.к. потеряется свзязь с массой ,которую нужно считать!!!
+
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
