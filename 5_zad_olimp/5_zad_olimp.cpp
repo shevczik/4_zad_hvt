615bf45680cecf2c458b773944c01ba45e53fddb
shevczik <94609661+shevczik@users.noreply.github.com>
2021-12-20 17:11:45 +0300

    Add files via upload

--- 5_zad_olimp/5_zad_olimp.cpp
+++ 5_zad_olimp/5_zad_olimp.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main()
 {
-    short x,y,amb_x =-1,amb_y=-1,size,koef;
+    short x,y,amb_x ,amb_y,size,koef;
     //1 строка
     cin >> y;
     cin >> x;
@@ -15,6 +15,8 @@ int main()
     cin >> amb_y;
     cin >> size;
     cin >> koef;
+    amb_x--;
+    amb_y--;
 
     vector <vector <int>> field_cost;
     field_cost.resize(x);
@@ -23,47 +25,54 @@ int main()
     }
     vector <vector<int>> field_mass = field_cost;
     vector <vector <int>> field_price = field_cost;
-    for (int i = 0; i < x; i++) { // заполнение вектора обьема клеток
-        for (int k = 0; k < y; k++) {
+    for (int i = 0; i < y; i++) { // заполнение вектора обьема клеток
+        for (int k = 0; k < x; k++) {
             short num;
             cin >> num;
-            field_mass[i][k] = num;
+            field_mass[k][i] = num;
         }
     }
 
-    for (int i = 0; i < x; i++) { // заполнение вектора цены за тону на клетке
-        for (int k = 0; k < y; k++) {
+    for (int i = 0; i < y; i++) { // заполнение вектора цены за тону на клетке
+        for (int k = 0; k < x; k++) {
             short num;
             cin >> num;
-            field_cost[i][k] = num;
+            field_cost[k][i] = num;
         }
     }
     
     for (int i = 0; i < x; i++) { // заполнение вектора стоимости пукупки + доставки на клетке
         for (int k = 0; k < y; k++) {
-            field_price [i][k] = field_cost[k][i]*field_mass[k][i];//цена покупки всего урожая
-            short dostavka=koef*(abs(amb_x-k)+abs(amb_y-i)); // цена доставки до амбара
+            field_price [i][k] = field_cost[i][k]*field_mass[i][k];//цена покупки всего урожая
+            unsigned short dostavka=koef*(abs(amb_x-k)+abs(amb_y-i)); // цена доставки до амбара
             field_price[i][k] += dostavka;
         }
     }
-    unsigned int cost = 0 ,mass=0;
+     short cost = 0 ,mass=0;
 
 
     short min=10000, min_x, min_y;
-    for (mass; mass < size;) {
+    for (; true;) {//поиск минимальных значений 
         for (int i = 0; i < x; i++) {
             for (int k = 0; k < y; k++) {
-                if (field_price[i][k] < min) {
-                    if (i != amb_x && k != amb_y) {
+                if (field_price[i][k] < min && field_price[i][k] != 0) {
                         min = field_price[i][k];
-                        field_price[i][k] = 100000;
-                    }
+                        min_x = i;
+                        min_y = k;
                 }
             }
         }
-        mass += min;
+        mass += field_mass[min_x][min_y];
+        field_price[min_x][min_y] = 10000;
+        if (mass > size) {
+            cout << cost;
+            return 1;
+        }
+        cost += min;
+        min = 100000;
+        
     }
-    cout << mass - min;
-    // сортировать ничего нельзя ,т.к. потеряется свзязь с массой ,которую нужно считать!!!
+    
+
 
 }
